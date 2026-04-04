c65a30f64e210a7d1132274c3c99aceb1fd0a81f
Annu9111 <aksoni911190@gmail.com>
2026-04-04 20:25:01 +0530

    views

--- shopora_project/store/views.py
+++ shopora_project/store/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from .models import Product
+from .models import Product, Category
 
 def home(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
+    categories = Category.objects.all()
+    data = []
+
+    for category in categories:
+        products = Product.objects.filter(category=category)[:3]
+        data.append({
+            'category': category,
+            'products': products
+        })
+
+    return render(request, 'home.html', {'data': data})
