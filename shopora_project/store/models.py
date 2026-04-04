746215a488a15c3968678ec767d2b5dd8c933ed9
Annu9111 <aksoni911190@gmail.com>
2026-04-04 16:36:31 +0530

    commit

--- shopora_project/store/models.py
+++ shopora_project/store/models.py
@@ -1,11 +1,18 @@
 from django.db import models
 
-# Create your models here.
-from django.db import models
+# Category first
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.name
+
+
+# Then Product
 class Product(models.Model):
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.IntegerField()
     description = models.TextField()
     image = models.ImageField(upload_to='products/')
 
