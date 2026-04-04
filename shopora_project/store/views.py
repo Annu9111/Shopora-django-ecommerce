1f11435b8fd3b34b4480dc2598efaa33d90a1465
Annu9111 <aksoni911190@gmail.com>
2026-04-04 14:44:14 +0530

    home.html

--- shopora_project/store/views.py
+++ shopora_project/store/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home(request):
-    return HttpResponse("Welcome to Shopora 🛒")
+    return render(request, 'store/home.html')
