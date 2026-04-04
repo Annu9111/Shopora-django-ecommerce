3aaf22a9436acfffd735680bf7ec915f41d4f558
Annu9111 <aksoni911190@gmail.com>
2026-04-04 20:43:24 +0530

    commit

--- shopora_project/store/views.py
+++ shopora_project/store/views.py
@@ -12,4 +12,4 @@ def home(request):
             'products': products
         })
 
-    return render(request, 'home.html', {'data': data})
+    return render(request, 'store/home.html', {'data': data})
