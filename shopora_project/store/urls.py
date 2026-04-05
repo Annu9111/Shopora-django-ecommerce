c5435708094b86f703c992606fc113515282516d
Annu9111 <aksoni911190@gmail.com>
2026-04-05 11:16:20 +0530

    commit

--- shopora_project/store/urls.py
+++ shopora_project/store/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
-    path('add-to-cart/<int:id>/', views.add_to_cart, name='add_to_cart'),
+    path('add-to-cart/<int:product_id>/', views.add_to_cart, name='add_to_cart'),
     path('cart/', views.cart_view, name='cart'),
     path('remove/<int:cart_id>/', views.remove_from_cart, name='remove_from_cart'),
 ]
