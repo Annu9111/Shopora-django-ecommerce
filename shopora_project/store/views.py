c67c5ce1f0398260c5753e35e082849237baa00d
Annu9111 <aksoni911190@gmail.com>
2026-04-05 11:19:49 +0530

    commit

--- shopora_project/store/views.py
+++ shopora_project/store/views.py
@@ -31,7 +31,7 @@ def add_to_cart(request, product_id):
         cart_item.quantity += 1
         cart_item.save()
 
-    return redirect('cart')
+    return redirect(request.META.get('HTTP_REFERER'))
 
 
 def cart_view(request):
